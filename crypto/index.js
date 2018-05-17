0c5aa9e50e705bfe85b9159138f98c36688a36bd
A477MZZ <robert.hanson@solutiondesign.com>
2018-05-17 08:59:57 -0500

    LetterCount example

--- crypto/index.js
+++ crypto/index.js
@@ -1,128 +1,44 @@
-var forge = require('node-forge');
-//forge.options.usePureJavaScript=true;
-
-var randomstring = require('randomstring');
 const js = import("./app_wasm");
 js.then(js => {
 
+    var letters = () => "abcdefghijklmnopqrstuvwxyz".split("").reduce((accum, letter) => { accum[letter] = 0; return accum; },{} );
 
-
-    // Client: Creates public/private key
-    // Client sends public key and text to service
-    // Service returns encrypted text, and encrypted symmetric key
-    // Client decrypts symmetric key using private key
-    // Client descrypts text using decrypted symmetric key
-
-    var text = generateText();
-    setFromText(text);
-    // We generate a public/private key
-
-    var rsa = forge.pki.rsa;
-    var keypair = rsa.generateKeyPair({bits: 2048, e: 0x10001});
-
-    // we send the public key and text
-    var publicKey_pem = forge.pki.publicKeyToPem(keypair.publicKey);
-    var encryptedData = Encrypt_Javascript(publicKey_pem, text);
-
-    var encryptedSymmetricKey = encryptedData.encryptedSymmetricKey;
-    var symmetricKey = keypair.privateKey.decrypt(encryptedSymmetricKey);
-    //var symmetricKey = encryptedData.symmetricKey;
-
-
-    debugger
-    var buffer = forge.util.createBuffer(encryptedData.encryptedText.bytes(), 'raw');
-    var plainText = Decrypt_Rust(symmetricKey, encryptedData.iv, buffer);
-    setToText(0, plainText);
-
-    /*
-    setTimeout(function() {
-        var plainText="xx";
-        var a = performance.now()
-        console.time('decrypt');
-        for (var i = 0; i < 500; i++) {
-            var buffer = forge.util.createBuffer(encryptedData.encryptedText.bytes(), 'raw');
-            plainText = Decrypt_Javascript(symmetricKey, encryptedData.iv, buffer);
-        }
-        var b = performance.now();
-        setToText(i, plainText);
-        setTimer(b-a);
-    },0)
-    */
-
-    /*
-    function process() {
-        let from = $('#from').val();
-        let to = $('#to').val();
-        let text = $('#text').val();
-        js.text_replace(from, to, text);
+    var letterReducer = (counts, letter) => {
+        counts[letter]=1;
+        return counts;
     }
 
-    $(function() {
-        $("input").keyup(process);
-    });
-    */
-})
+    var wordReducer = (counts, word) => {
+        var wordCounts = word.split("").reduce(letterReducer, letters());
+        var result = Object.keys(counts).reduce((accum, key) => { accum[key] = counts[key] + wordCounts[key]; return accum },{} )
+        return result;
+    }
 
-function generateText() {
+    var letterCounts = (str) => {
+        var words = str.split(" ");
+        var wordsLetterCount = words.reduce(wordReducer, letters());
+        return wordsLetterCount;
+        //return Promise.resolve(wordsLetterCount)
+    }
 
-    var s = "";
-    for (var i = 0; i < 10000; i++)
-    s = s + randomstring.generate({
-        length: 8,
-        charset: 'alphabetic'
-    }) +' '
-    return s;
-}
+    //console.log('xxx', wordReducer(letters(), "abcf"));
+    //console.log(letterCounts("abcf bcf ag"));
 
-function setFromText(str) {
-    $('#from').text(str);
-}
+    fetch('./MobyDick.txt')
+        .then(response => response.text())
+        .then(text => {
+            var a = performance.now()
+            var counts = letterCounts(text);
+            var b = performance.now();
+            showResults(counts);
+            showElapsed(b-a);
+        })
+});
 
-function setToText(iteration, str) {
-    $('#iteration').text(iteration);
-    $('#to').text(str);
-}
-
-function setTimer(timer) {
-    $('#timer').text(timer)
-}
-
-function Encrypt_Javascript(publicKey_pem, text) {
-    // generate a random key and IV
-    // Note: a key size of 16 bytes will use AES-128, 24 => AES-192, 32 => AES-256
-    var symmetricKey = forge.random.getBytesSync(32);
-    var iv = forge.random.getBytesSync(16);
-
-    var publicKey = forge.pki.publicKeyFromPem(publicKey_pem);
-    var encryptedSymmetricKey = publicKey.encrypt(symmetricKey);
-
-    // encrypt some bytes using CBC mode
-    // (other modes include: CFB, OFB, CTR, and GCM)
-    var cipher = forge.cipher.createCipher('AES-CBC', symmetricKey);
-    cipher.start({iv: iv});
-    cipher.update(new forge.util.ByteBuffer(text, 'utf8'));
-    cipher.finish();
-    var encryptedText = cipher.output;
-    return { symmetricKey: symmetricKey, encryptedSymmetricKey: encryptedSymmetricKey, encryptedText: encryptedText, iv: iv };
-}
-
-function Decrypt_Javascript(symmetricKey, iv, encryptedText)
-{
-    var decipher = forge.cipher.createDecipher('AES-CBC', symmetricKey);
-    decipher.start({iv: iv});
-    decipher.update(encryptedText);
-    decipher.finish();
-    var plainText = decipher.output.toString('utf8')
-    return plainText;
-}
-
-function Decrypt_Rust(symmetricKey, iv, encryptedText)
-{
-    var plainText = decrypt_rust(symmetricKey, iv, encryptedText)
-    return plainText;
-}
-
-export function update_text(s) {
+export function showResults(s) {
     $('#result').text(s);
+}
+export function showElapsed(s) {
+    $('#elapsed').text(s);
 }
 
--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -1,47 +1,146 @@
 #![feature(proc_macro, wasm_custom_section, wasm_import_module)]
 
 extern crate wasm_bindgen;
-extern crate crypto;
-
 
 use wasm_bindgen::prelude::*;
-use crypto::digest::Digest;
-use crypto::sha2::Sha256;
 
 #[wasm_bindgen(module = "./index")]
 extern {
-    fn update_text(s: &str);
+     fn update(results: &Vec<Counter>);
 }
 
-#[wasm_bindgen]
-pub fn decrypt_rust(symmetricKey: &[u8], iv: &[u8], encryptedText: &[u8]) {
-    let decryptedArray = decrypt_block(encryptedText, symmetricKey,iv);
-    let decrypted = str::from_utf8(decryptedArray).unwrap();
-    decrypted;
+#[derive(Debug)]
+pub struct Counter {
+    key: char,
+    count: i32
+}
+impl Counter {
+        pub fn new(key: char, count: i32) -> Counter {
+            Counter { key: key, count: count }
+        }
 }
 
-fn decrypt_block(block: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>, symmetriccipher::SymmetricCipherError> {
-    let mut decryptor: Box<symmetriccipher::Decryptor> = aes::cbc_decryptor(
-            aes::KeySize::KeySize256,
-            key,
-            iv,
-            blockmodes::PkcsPadding
-    );
+fn letters() -> Vec<Counter> {
+    let s = "abcdefghijklmnopqrstuvwxyz";
+    let mut vec: Vec<Counter> = Vec::new();
+    for (i, c) in s.chars().enumerate() {
+        vec.push(Counter::new(c,0));
+    }
+    vec
+}
 
-    let mut final_result = Vec::<u8>::new();
-    let mut buffer = [0, ..4096];
-    let mut read_buffer = buffer::RefReadBuffer::new(block);
-    let mut write_buffer = buffer::RefWriteBuffer::new(&mut buffer);
+fn combine(a: &Vec<Counter>, b: &Vec<Counter>) -> Vec<Counter> {
+    let zipiter = a.iter().zip(b.iter());
+    zipiter.map(|(aelem, belem)| Counter::new(aelem.key, aelem.count + belem.count))
+    .collect()
+}
 
-    loop {
-        let result = try!(decryptor.decrypt(&mut read_buffer, &mut write_buffer, true));
-        final_result.push_all(write_buffer.take_read_buffer().take_remaining());
-        match result {
-            buffer::BufferResult::BufferUnderflow => break,
-            buffer::BufferResult::BufferOverflow => { }
+fn wordReducer(wordCounts: Vec<Counter>, word: &str) -> Vec<Counter> {
+    let mut letterCounts = letters();
+    for (i, c) in word.chars().enumerate() {
+        if let Some(pos) = letterCounts.iter().position(|ref x| x.key == c) {
+            let index = pos;
+            if let Some(elem) = letterCounts.get_mut(index) {
+                (*elem).count = 1;
+            }
         }
     }
-
-    Ok(final_result)
+    /*
+    println!("------");
+    println!("After wordReducer: {}", word);
+    println!("wordCounts {:?}", wordCounts);
+    println!("letterCounts {:?}", letterCounts);
+    println!("combined {:?}", combine(&wordCounts, &letterCounts));
+    println!("");
+    */
+    combine(&wordCounts, &letterCounts)
 }
 
+/* a method we can call from javascript */
+#[wasm_bindgen]
+pub fn countLettersInWords(str: &str) -> Vec<Counter>{
+    let lcase_str = str.to_lowercase();
+   let words: Vec<&str> = lcase_str
+    .split(" ")
+    .collect();
+   let wordLetterCounts = letters();
+   let counts = words.iter().fold(wordLetterCounts, |accum, word| wordReducer(accum, word));
+   counts
+}
+
+
+
+
+#[test]
+fn counter_new() {
+    let c = Counter::new('a',3);
+    assert_eq!('a', c.key);
+}
+
+#[test]
+fn counter_new_with_addition() {
+    let a = Counter::new('a',2);
+    let b = Counter::new('a',4);
+    let c = Counter::new('a', a.count + b.count);
+    assert_eq!('a', c.key);
+}
+
+#[test]
+fn letters_test() {
+    let l = letters();
+    assert_eq!('a', l[0].key);
+    assert_eq!(0, l[0].count);
+    assert_eq!(26, l.len());
+}
+
+#[test]
+fn combine_test() {
+    let mut a = letters();
+    let mut b = letters();
+    a[0] = Counter::new('a',12);
+    a[1] = Counter::new('b',4);
+    b[1] = Counter::new('b',3);
+    let c = combine(&a,&b);
+    assert_eq!(12, c[0].count);
+    assert_eq!(7, c[1].count)
+}
+
+#[test]
+fn wordreducer_test() {
+    let wordCounts = letters();
+    let v = wordReducer(wordCounts, "abc");
+    assert_eq!(1, v[0].count);
+}
+#[test]
+fn wordreducer_test2() {
+    let wordCounts = letters();
+    let v = wordReducer(wordCounts, "a");
+    let v2 = wordReducer(v,"a");
+    assert_eq!(2, v2[0].count);
+}
+
+#[test]
+fn countLettersInWords_test() {
+    let count = countLettersInWords("a");
+    assert_eq!('a', count[0].key);
+    assert_eq!(1, count[0].count);
+}
+#[test]
+fn countLettersInWords_test2() {
+    let count = countLettersInWords("a a");
+    assert_eq!('a', count[0].key);
+    assert_eq!(2, count[0].count);
+}
+#[test]
+fn countLettersInWords_test3() {
+    let count = countLettersInWords("ab ab");
+    assert_eq!('a', count[0].key);
+    assert_eq!(2, count[0].count);
+}
+
+#[test]
+fn countLettersInWords_test_mixed_upper_and_lower() {
+    let count = countLettersInWords("a A a A");
+    assert_eq!('a', count[0].key);
+    assert_eq!(4, count[0].count);
+}
