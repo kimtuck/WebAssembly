71b52bf0de0619ccfd682bd390de029bd20bfc3a
kimtuck <Treasure2012>
2018-07-19 09:18:37 -0500

    Prep for moving branch into master

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-gulp = require('gulp');
-
-gulp.task('copy', function() {
-    gulp.src('./target/wasm32-unknown-emscripten/release/deps/*.wasm')
-        .pipe(gulp.dest('./site'));
-    gulp.src('./target/wasm32-unknown-emscripten/release/deps/*.js')
-        .pipe(gulp.dest('./site'));
-});
