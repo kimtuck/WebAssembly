1c60bc47778a0faeb682610572bbc79ce5663b32
kimtuck <Treasure2012>
2018-07-19 08:58:17 -0500

    WIP

--- hello-world/app_wasm.js
+++ hello-world/app_wasm.js
@@ -7,42 +7,21 @@ const TextDecoder = typeof self === 'object' && self.TextDecoder
 
 let cachedDecoder = new TextDecoder('utf-8');
 
-let cachedUint8Memory = null;
+let cachegetUint8Memory = null;
 function getUint8Memory() {
-    if (cachedUint8Memory === null ||
-        cachedUint8Memory.buffer !== wasm.memory.buffer)
-        cachedUint8Memory = new Uint8Array(wasm.memory.buffer);
-    return cachedUint8Memory;
+    if (cachegetUint8Memory === null ||
+        cachegetUint8Memory.buffer !== wasm.memory.buffer)
+        cachegetUint8Memory = new Uint8Array(wasm.memory.buffer);
+    return cachegetUint8Memory;
 }
 
 function getStringFromWasm(ptr, len) {
-    return cachedDecoder.decode(getUint8Memory().slice(ptr, ptr + len));
+    return cachedDecoder.decode(getUint8Memory().subarray(ptr, ptr + len));
 }
 
-let cachedUint32Memory = null;
-function getUint32Memory() {
-    if (cachedUint32Memory === null ||
-        cachedUint32Memory.buffer !== wasm.memory.buffer)
-        cachedUint32Memory = new Uint32Array(wasm.memory.buffer);
-    return cachedUint32Memory;
-}
-
-let cachedGlobalArgumentPtr = null;
-function globalArgumentPtr() {
-    if (cachedGlobalArgumentPtr === null)
-        cachedGlobalArgumentPtr = wasm.__wbindgen_global_argument_ptr();
-    return cachedGlobalArgumentPtr;
-}
-
-function getGlobalArgument(arg) {
-    const idx = globalArgumentPtr() / 4 + arg;
-    return getUint32Memory()[idx];
-}
-
-export function __wbg_f_alert_alert_n(arg0) {
-    let len0 = getGlobalArgument(0);
-    let v0 = getStringFromWasm(arg0, len0);
-    alert(v0);
+export function __wbg_f_alert_alert_n(arg0, arg1) {
+    let varg0 = getStringFromWasm(arg0, arg1);
+    alert(varg0);
 }
 
 const TextEncoder = typeof self === 'object' && self.TextEncoder
@@ -59,16 +38,10 @@ function passStringToWasm(arg) {
     return [ptr, buf.length];
 }
 
-function setGlobalArgument(arg, i) {
-    const idx = globalArgumentPtr() / 4 + i;
-    getUint32Memory()[idx] = arg;
-}
-
 export function greet(arg0) {
     const [ptr0, len0] = passStringToWasm(arg0);
-    setGlobalArgument(len0, 0);
     try {
-        return wasm.greet(ptr0);
+        return wasm.greet(ptr0, len0);
     } finally {
         wasm.__wbindgen_free(ptr0, len0 * 1);
     }
